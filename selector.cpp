ad629aa6c6b49c93e21f71fd5d036ac5aa3818bd
Kotyubeev Roman <favorshlitex at gmail dot com>
2022-03-17 12:22:44 +0700

    changed container (from vector to list) to store fds

--- selector.cpp
+++ selector.cpp
@@ -27,12 +27,8 @@ void SessionSelector::Run()
 	int sret;
 	fd_set readfds;
 	for (;;) {
-		std::vector<int>::size_type i;
-		std::vector<int>::size_type size = fds.size();
-
-		FD_ZERO(&readfds);
-		for (i = 0; i < size; i++)
-			FD_SET(fds[i], &readfds);
+		for (int fd : fds)
+			FD_SET(fd, &readfds);
 		
 		sret = select(max_fd+1, &readfds, 0, 0, 0);
 		if (sret == -1) {
@@ -42,9 +38,9 @@ void SessionSelector::Run()
 				break;
 		}
 		if (sret > 0) {
-			for (i = 0; i < size; i++)
-				if (FD_ISSET(fds[i], &readfds))
-					fdobj_map[fds[i]]->Handle();
+			for (int fd : fds)
+				if (FD_ISSET(fd, &readfds))
+					fdobj_map[fd]->Handle();
 		}
 	}
 }
--- selector.hpp
+++ selector.hpp
@@ -1,7 +1,7 @@
 #ifndef SESSION_SELECTOR_HPP_ENTRY
 #define SESSION_SELECTOR_HPP_ENTRY
 #include <map>
-#include <vector>
+#include <list>
 
 #include "fdobj.hpp"
 
@@ -12,13 +12,13 @@ enum {
 class SessionSelector {
 	friend class Server;
 	std::map<int, FdObj*> fdobj_map;
-	std::vector<int> fds;
+	std::list<int> fds;
 	int max_fd;
 public:
 	SessionSelector() : max_fd(-1) {}
 	~SessionSelector() {}
 	void Run();
-	void Add(FdObj *fdobj);
+	void Add(FdObj *fdobj, int sockfd);
 	void Remove(FdObj *fdobj);
 };
 
